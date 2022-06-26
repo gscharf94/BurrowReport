2ba9855f28375457c2ea25de674c0ba6d5d3f858
Gustavo Scharf <scharfgustavo@gmail.com>
2022-06-26 01:36:12 -0400

    user interface on inputProduction coming together. need to start with the logic and mechanics of drawing stuff soon

--- final/public/scripts/inputProduction.js
+++ final/public/scripts/inputProduction.js
@@ -4,6 +4,10 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const leaflet_1 = __importDefault(require("leaflet"));
+window.addBoreStart = addBoreStart;
+window.addRockStart = addRockStart;
+window.addVaultStart = addVaultStart;
+window.cancelClick = cancelClick;
 let map = leaflet_1.default.map('map').setView([0, 0], 5);
 leaflet_1.default.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'BLUEOCEAN',
@@ -13,3 +17,115 @@ leaflet_1.default.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}
     zoomOffset: -1,
     accessToken: 'pk.eyJ1IjoiZ3NjaGFyZjk0IiwiYSI6ImNreWd2am9mODBjbnMyb29sNjZ2Mnd1OW4ifQ.1cSadM_VR54gigTAsVVGng'
 }).addTo(map);
+/**
+ * just housekeeping stuff so that I don't have it scattered throughout
+ * the file and all in one place
+ *
+ * @returns {void}
+ */
+function initialization() {
+    const elementsToHide = [
+        'dateLabel', 'dateInput',
+        'footageLabel', 'footageInput',
+        'vaultLabel', 'vaultSelect',
+        'cancel', 'submit',
+    ];
+    hideAndShowElements([], elementsToHide);
+}
+/**
+ * takes in two lists of element ids
+ * it removes the 'hide' class from the first ones
+ * and adds it to the second ones
+ *
+ * @param {string[]} toShow - string[] - ids for elements to show
+ * @param {string[]} toHide - string[] - ids for elements to hide
+ * @returns {void}
+ */
+function hideAndShowElements(toShow, toHide) {
+    toShow.map((id) => {
+        let element = document.getElementById(id);
+        element.classList.remove('hide');
+    });
+    toHide.map((id) => {
+        let element = document.getElementById(id);
+        element.classList.add('hide');
+    });
+}
+/**
+ * the user has clicked on the add bore button so now we start the process
+ * of adding a bore...
+ * 1 - we show/hide the correct elements
+ *
+ * @returns {void}
+ */
+function addBoreStart() {
+    const elementsToShow = [
+        'footageLabel', 'footageInput',
+        'dateLabel', 'dateInput',
+        'cancel', 'submit'
+    ];
+    const elementsToHide = [
+        'vaultLabel', 'vaultSelect',
+        'addRock', 'addVault',
+    ];
+    hideAndShowElements(elementsToShow, elementsToHide);
+}
+/**
+ * the user has clicked on the add rock button so now we start the process
+ * of adding a bore...
+ * 1 - we show/hide the correct elements
+ *
+ * @returns {void}
+ */
+function addRockStart() {
+    const elementsToShow = [
+        'footageLabel', 'footageInput',
+        'dateLabel', 'dateInput',
+        'cancel', 'submit'
+    ];
+    const elementsToHide = [
+        'vaultLabel', 'vaultSelect',
+        'addBore', 'addVault',
+    ];
+    hideAndShowElements(elementsToShow, elementsToHide);
+}
+/**
+ * the user has clicked on the add vault button so now we start the process
+ * of adding a bore...
+ * 1 - we show/hide the correct elements
+ *
+ * @returns {void}
+ */
+function addVaultStart() {
+    const elementsToShow = [
+        'dateLabel', 'dateInput',
+        'vaultLabel', 'vaultSelect',
+        'cancel', 'submit',
+    ];
+    const elementsToHide = [
+        'footageLabel', 'footageInput',
+        'addRock', 'addBore',
+    ];
+    hideAndShowElements(elementsToShow, elementsToHide);
+}
+/**
+ * user clicks cancel, so we need to show/hide the proper elements
+ * as well as fix the logic to restart everthing
+ *
+ * TO DO
+ *
+ * @returns {void}
+ */
+function cancelClick() {
+    const elementsToShow = [
+        'addBore', 'addVault', 'addRock'
+    ];
+    const elementsToHide = [
+        'footageLabel', 'footageInput',
+        'vaultLabel', 'vaultSelect',
+        'dateLabel', 'dateInput',
+        'cancel', 'submit',
+    ];
+    hideAndShowElements(elementsToShow, elementsToHide);
+}
+initialization();
--- src/public/scripts/inputProduction.ts
+++ src/public/scripts/inputProduction.ts
@@ -1,5 +1,19 @@
 import L from 'leaflet';
 
+declare global {
+  interface Window {
+    addBoreStart : () => void,
+    addRockStart : () => void,
+    addVaultStart : () => void,
+    cancelClick : () => void,
+  }
+}
+
+window.addBoreStart = addBoreStart;
+window.addRockStart = addRockStart;
+window.addVaultStart = addVaultStart;
+window.cancelClick = cancelClick;
+
 let map = L.map('map').setView([0, 0], 5);
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
   attribution: 'BLUEOCEAN',
@@ -9,3 +23,124 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
   zoomOffset: -1,
   accessToken: 'pk.eyJ1IjoiZ3NjaGFyZjk0IiwiYSI6ImNreWd2am9mODBjbnMyb29sNjZ2Mnd1OW4ifQ.1cSadM_VR54gigTAsVVGng'
 }).addTo(map);
+
+
+/**
+ * just housekeeping stuff so that I don't have it scattered throughout
+ * the file and all in one place
+ *
+ * @returns {void}
+ */
+function initialization() : void {
+  const elementsToHide = [
+    'dateLabel', 'dateInput',
+    'footageLabel', 'footageInput',
+    'vaultLabel', 'vaultSelect',
+    'cancel', 'submit',
+  ];
+  hideAndShowElements([], elementsToHide);
+}
+
+/**
+ * takes in two lists of element ids
+ * it removes the 'hide' class from the first ones 
+ * and adds it to the second ones
+ *
+ * @param {string[]} toShow - string[] - ids for elements to show
+ * @param {string[]} toHide - string[] - ids for elements to hide
+ * @returns {void}
+ */
+function hideAndShowElements(toShow : string[], toHide : string[]) : void {
+  toShow.map((id) => {
+    let element = document.getElementById(id);
+    element.classList.remove('hide');
+  });
+  toHide.map((id) => {
+    let element = document.getElementById(id);
+    element.classList.add('hide');
+  })
+}
+
+
+/**
+ * the user has clicked on the add bore button so now we start the process
+ * of adding a bore... 
+ * 1 - we show/hide the correct elements
+ *
+ * @returns {void}
+ */
+function addBoreStart() : void {
+  const elementsToShow = [
+    'footageLabel', 'footageInput',
+    'dateLabel', 'dateInput',
+    'cancel', 'submit'
+  ];
+  const elementsToHide = [
+    'vaultLabel', 'vaultSelect',
+    'addRock', 'addVault',
+  ];
+  hideAndShowElements(elementsToShow, elementsToHide);
+}
+
+/**
+ * the user has clicked on the add rock button so now we start the process
+ * of adding a bore... 
+ * 1 - we show/hide the correct elements
+ *
+ * @returns {void}
+ */
+function addRockStart() : void {
+  const elementsToShow = [
+    'footageLabel', 'footageInput',
+    'dateLabel', 'dateInput',
+    'cancel', 'submit'
+  ];
+  const elementsToHide = [
+    'vaultLabel', 'vaultSelect',
+    'addBore', 'addVault',
+  ];
+  hideAndShowElements(elementsToShow, elementsToHide);
+}
+
+/**
+ * the user has clicked on the add vault button so now we start the process
+ * of adding a bore... 
+ * 1 - we show/hide the correct elements
+ *
+ * @returns {void}
+ */
+function addVaultStart() : void {
+  const elementsToShow = [
+    'dateLabel', 'dateInput',
+    'vaultLabel', 'vaultSelect',
+    'cancel', 'submit',
+  ];
+  const elementsToHide = [
+    'footageLabel', 'footageInput',
+    'addRock', 'addBore',
+  ];
+  hideAndShowElements(elementsToShow, elementsToHide);
+}
+
+/**
+ * user clicks cancel, so we need to show/hide the proper elements
+ * as well as fix the logic to restart everthing
+ *
+ * TO DO 
+ *
+ * @returns {void}
+ */
+function cancelClick() : void {
+  const elementsToShow = [
+    'addBore', 'addVault', 'addRock'
+  ];
+  const elementsToHide = [
+    'footageLabel', 'footageInput',
+    'vaultLabel', 'vaultSelect',
+    'dateLabel', 'dateInput',
+    'cancel', 'submit',
+  ]
+  hideAndShowElements(elementsToShow, elementsToHide);
+}
+
+initialization();
