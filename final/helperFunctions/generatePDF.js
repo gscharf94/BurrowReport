ff46646a71d863f2bf746d4ca332ff9902c80adf
Gustavo Scharf <scharfgustavo@gmail.com>
2022-08-20 01:42:23 -0400

    bores <800 are working perfectly.. we need to make it so it can create >800 bores and also string together various bores and combine them

--- final/helperFunctions/generatePDF.js
+++ final/helperFunctions/generatePDF.js
@@ -5,28 +5,94 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const pdfkit_1 = __importDefault(require("pdfkit"));
 const fs_1 = __importDefault(require("fs"));
-const doc = new pdfkit_1.default();
+const doc = new pdfkit_1.default({ autoFirstPage: false });
+doc.addPage({ margin: 0 });
 const firaRegular = 'final/fonts/FiraMono-Regular.ttf';
 const firaMedium = 'final/fonts/FiraMono-Medium.ttf';
 const firaBold = 'final/fonts/FiraMono-Bold.ttf';
+const testingBores = generateTestingBores(746);
 doc.pipe(fs_1.default.createWriteStream('testing.pdf'));
-const rows = [
-    { ft: 3, inches: 3 },
-    { ft: 3, inches: 5 },
-    { ft: 3, inches: 2 },
-    { ft: 3, inches: 7 },
-    { ft: 3, inches: 8 },
-    { ft: 3, inches: 11 },
-    { ft: 4, inches: 3 },
-];
 const info = {
     crew_name: 'test_crew',
     work_date: '2022-08-21',
     job_name: 'P4745',
-    page_number: '2',
+    bore_number: '1',
     client_name: 'Danella',
     billing_code: 'A1',
 };
+function writeBoreDepthHeaders() {
+    doc.font(firaMedium).fontSize(15);
+    doc.text('Rod', 50, 95);
+    doc.text('Depth', 125, 95);
+    doc.text('EOP', 225, 95);
+    doc.text('Rod', 300, 95);
+    doc.text('Depth', 375, 95);
+    doc.text('EOP', 475, 95);
+}
+function drawGrayRectangles() {
+    const startingX = 40;
+    const startingY = 119;
+    const width = 500;
+    const height = 16;
+    const color = {
+        r: 210,
+        g: 210,
+        b: 210,
+    };
+    const opacity = 1;
+    const rowHeight = 32;
+    doc.fillColor([color.r, color.g, color.b], opacity);
+    for (let i = 0; i < 20; i++) {
+        let y = startingY + (rowHeight * i);
+        doc.rect(startingX, y, width, height).fill();
+    }
+    doc.fillColor('black', 1);
+}
+function writeBoresToPage(bores) {
+    const xFirstColumn = 50;
+    const xSecondColumn = 300;
+    const startingY = 119;
+    const rowGap = 16;
+    const fontSize = 14;
+    let counter = 10;
+    const formatCounter = (n) => {
+        return String(n).padStart(3, "0");
+    };
+    let [x, y] = [xFirstColumn, startingY];
+    doc.font(firaRegular).fontSize(fontSize);
+    for (let i = 0; i < bores.length; i++) {
+        if (i % 40 == 0 && i != 0) {
+            x = xSecondColumn;
+            y = startingY;
+        }
+        let ftg = String(bores[i].ft).padStart(2, "0");
+        let inches = String(bores[i].inches).padStart(2, "0");
+        doc.text(`${formatCounter(counter)}      ${ftg}'${inches}"`, x, y);
+        y += rowGap;
+        counter += 10;
+    }
+}
+function generateTestingBores(ftg) {
+    let output = [];
+    let numOfRows = Math.floor(ftg / 10);
+    if (ftg % 10 !== 0) {
+        numOfRows++;
+    }
+    for (let i = 0; i < numOfRows; i++) {
+        let ftg = Math.floor((Math.random() * 3) + 2);
+        let inches = Math.floor((Math.random() * 12));
+        output.push({
+            ft: ftg,
+            inches: inches,
+        });
+    }
+    return output;
+}
+function splitArrayIntoSetsOf80(depths) {
+    const n = 80;
+    let output = [];
+    return output;
+}
 function writeText(text, font, size, x, y) {
     doc
         .font(font)
@@ -35,31 +101,38 @@ function writeText(text, font, size, x, y) {
 }
 function writeEmptyHeader() {
     doc.font(firaBold).fontSize(15);
-    doc.text('Crew Name:', 50, 50);
-    doc.text('Work Date:', 50, 70);
-    doc.text(' Job Name:', 50, 90);
-    doc.text(' Client Name:', 300, 50);
-    doc.text('Billing Code:', 300, 70);
-    doc.text(' Page Number:', 300, 90);
+    doc.text('Crew Name:', 50, 20);
+    doc.text('Work Date:', 50, 40);
+    doc.text(' Job Name:', 50, 60);
+    doc.text(' Client Name:', 300, 20);
+    doc.text('Billing Code:', 300, 40);
+    doc.text(' Bore Number:', 300, 60);
 }
-function drawHeaderLine() {
-    doc.moveTo(45, 115)
-        .lineTo(510, 115)
+function drawHeaderLines() {
+    doc.moveTo(45, 85)
+        .lineTo(510, 85)
         .lineWidth(3)
+        .stroke();
+    doc.moveTo(275, 95)
+        .lineTo(275, 762)
+        .lineWidth(1)
         .stroke();
 }
 function writeHeader(info) {
     writeEmptyHeader();
     doc.font(firaRegular).fontSize(14);
-    doc.text(info.crew_name, 165, 50);
-    doc.text(info.work_date, 165, 70);
-    doc.text(info.job_name, 165, 90);
-    doc.text(info.client_name, 440, 50);
-    doc.text(info.billing_code, 440, 70);
-    doc.text(info.page_number, 440, 90);
-    drawHeaderLine();
+    doc.text(info.crew_name, 165, 20);
+    doc.text(info.work_date, 165, 40);
+    doc.text(info.job_name, 165, 60);
+    doc.text(info.client_name, 440, 20);
+    doc.text(info.billing_code, 440, 40);
+    doc.text(info.bore_number, 440, 60);
+    drawHeaderLines();
 }
+drawGrayRectangles();
 writeHeader(info);
+writeBoreDepthHeaders();
+writeBoresToPage(testingBores);
 // let i = 0;
 // for (const row of rows) {
 //   doc.text(`${row.ft}' ${row.inches}"`, 10, 20 * i++);
--- src/helperFunctions/generatePDF.ts
+++ src/helperFunctions/generatePDF.ts
@@ -1,29 +1,114 @@
 import PDFDocument from 'pdfkit';
 import fs from 'fs';
 
-const doc = new PDFDocument();
+const doc = new PDFDocument({ autoFirstPage: false });
+doc.addPage({ margin: 0 });
 const firaRegular = 'final/fonts/FiraMono-Regular.ttf';
 const firaMedium = 'final/fonts/FiraMono-Medium.ttf';
 const firaBold = 'final/fonts/FiraMono-Bold.ttf';
+const testingBores = generateTestingBores(746);
 doc.pipe(fs.createWriteStream('testing.pdf'));
 
-const rows = [
-  { ft: 3, inches: 3 },
-  { ft: 3, inches: 5 },
-  { ft: 3, inches: 2 },
-  { ft: 3, inches: 7 },
-  { ft: 3, inches: 8 },
-  { ft: 3, inches: 11 },
-  { ft: 4, inches: 3 },
-]
+type BoreDepth = { ft : number, inches : number };
+
 
 const info = {
   crew_name: 'test_crew',
   work_date: '2022-08-21',
   job_name: 'P4745',
-  page_number: '2',
+  bore_number: '1',
   client_name: 'Danella',
   billing_code: 'A1',
+}
+
+function writeBoreDepthHeaders() {
+  doc.font(firaMedium).fontSize(15);
+  doc.text('Rod', 50, 95);
+  doc.text('Depth', 125, 95);
+  doc.text('EOP', 225, 95);
+
+  doc.text('Rod', 300, 95);
+  doc.text('Depth', 375, 95);
+  doc.text('EOP', 475, 95);
+
+}
+
+function drawGrayRectangles() {
+  const startingX = 40;
+  const startingY = 119;
+  const width = 500;
+  const height = 16;
+  const color = {
+    r: 210,
+    g: 210,
+    b: 210,
+  }
+  const opacity = 1;
+  const rowHeight = 32;
+
+  doc.fillColor([color.r, color.g, color.b], opacity);
+
+  for (let i = 0; i < 20; i++) {
+    let y = startingY + (rowHeight * i);
+    doc.rect(startingX, y, width, height).fill();
+  }
+
+
+  doc.fillColor('black', 1);
+}
+
+function writeBoresToPage(bores : BoreDepth[]) {
+  const xFirstColumn = 50;
+  const xSecondColumn = 300;
+  const startingY = 119;
+  const rowGap = 16;
+  const fontSize = 14;
+
+  let counter = 10;
+  const formatCounter = (n : number) => {
+    return String(n).padStart(3, "0");
+  }
+
+
+  let [x, y] = [xFirstColumn, startingY];
+  doc.font(firaRegular).fontSize(fontSize);
+  for (let i = 0; i < bores.length; i++) {
+    if (i % 40 == 0 && i != 0) {
+      x = xSecondColumn;
+      y = startingY;
+    }
+    let ftg = String(bores[i].ft).padStart(2, "0");
+    let inches = String(bores[i].inches).padStart(2, "0");
+    doc.text(`${formatCounter(counter)}      ${ftg}'${inches}"`, x, y);
+    y += rowGap;
+    counter += 10;
+  }
+}
+
+function generateTestingBores(ftg : number) : BoreDepth[] {
+  let output = [];
+
+  let numOfRows = Math.floor(ftg / 10);
+  if (ftg % 10 !== 0) {
+    numOfRows++;
+  }
+
+  for (let i = 0; i < numOfRows; i++) {
+    let ftg = Math.floor((Math.random() * 3) + 2);
+    let inches = Math.floor((Math.random() * 12));
+    output.push({
+      ft: ftg,
+      inches: inches,
+    });
+  }
+
+  return output;
+}
+
+function splitArrayIntoSetsOf80(depths : BoreDepth[]) : BoreDepth[][] {
+  const n = 80;
+  let output = [];
+  return output;
 }
 
 function writeText(text : string, font : string, size : number, x : number, y : number) {
@@ -35,34 +120,42 @@ function writeText(text : string, font : string, size : number, x : number, y : 
 
 function writeEmptyHeader() {
   doc.font(firaBold).fontSize(15);
-  doc.text('Crew Name:', 50, 50);
-  doc.text('Work Date:', 50, 70);
-  doc.text(' Job Name:', 50, 90);
-  doc.text(' Client Name:', 300, 50);
-  doc.text('Billing Code:', 300, 70);
-  doc.text(' Page Number:', 300, 90);
+  doc.text('Crew Name:', 50, 20);
+  doc.text('Work Date:', 50, 40);
+  doc.text(' Job Name:', 50, 60);
+  doc.text(' Client Name:', 300, 20);
+  doc.text('Billing Code:', 300, 40);
+  doc.text(' Bore Number:', 300, 60);
 }
 
-function drawHeaderLine() {
-  doc.moveTo(45, 115)
-    .lineTo(510, 115)
+function drawHeaderLines() {
+  doc.moveTo(45, 85)
+    .lineTo(510, 85)
     .lineWidth(3)
+    .stroke();
+
+  doc.moveTo(275, 95)
+    .lineTo(275, 762)
+    .lineWidth(1)
     .stroke();
 }
 
 function writeHeader(info) {
   writeEmptyHeader();
   doc.font(firaRegular).fontSize(14);
-  doc.text(info.crew_name, 165, 50);
-  doc.text(info.work_date, 165, 70);
-  doc.text(info.job_name, 165, 90);
-  doc.text(info.client_name, 440, 50);
-  doc.text(info.billing_code, 440, 70);
-  doc.text(info.page_number, 440, 90);
-  drawHeaderLine();
+  doc.text(info.crew_name, 165, 20);
+  doc.text(info.work_date, 165, 40);
+  doc.text(info.job_name, 165, 60);
+  doc.text(info.client_name, 440, 20);
+  doc.text(info.billing_code, 440, 40);
+  doc.text(info.bore_number, 440, 60);
+  drawHeaderLines();
 }
 
+drawGrayRectangles();
 writeHeader(info);
+writeBoreDepthHeaders();
+writeBoresToPage(testingBores);
 
 // let i = 0;
 // for (const row of rows) {
