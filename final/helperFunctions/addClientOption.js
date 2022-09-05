55756ebe187ea57e6000658e56c40a54c6e420dd
Gustavo Scharf <scharfgustavo@gmail.com>
2022-09-05 02:45:19 -0400

    added helper function to add client options

---
+++ final/helperFunctions/addClientOption.js
@@ -0,0 +1,21 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const db_js_1 = require("../db.js");
+const clientName = "Future";
+const mapObjectType = "LINE"; // "LINE" | "MARKER"
+const primaryColor = "blue";
+const billingCode = "COM0001";
+const billingDescription = "4-way Bore";
+const dashed = false;
+let query = `
+  INSERT INTO client_options(
+    client_name, map_object_type, primary_color,
+    billing_code, billing_description, dashed
+  )
+  VALUES (
+    '${clientName}', ${mapObjectType},
+    '${primaryColor}','${billingCode}' ,
+    '${billingDescription}', ${dashed}
+  );
+`;
+db_js_1.pool.query(query);
---
+++ src/helperFunctions/addClientOption.ts
@@ -0,0 +1,23 @@
+import { pool } from '../db.js';
+
+const clientName = "Future";
+const mapObjectType = "LINE"; // "LINE" | "MARKER"
+const primaryColor = "blue";
+const billingCode = "COM0001";
+const billingDescription = "4-way Bore";
+const dashed = false;
+
+
+let query = `
+  INSERT INTO client_options(
+    client_name, map_object_type, primary_color,
+    billing_code, billing_description, dashed
+  )
+  VALUES (
+    '${clientName}', ${mapObjectType},
+    '${primaryColor}','${billingCode}' ,
+    '${billingDescription}', ${dashed}
+  );
+`;
+
+pool.query(query);
